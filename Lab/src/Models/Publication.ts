dc46d92cbd73357ed7dcc0e5d4e7d82f31603422
ghada-njimi <njimighadaa28@gmail.com>
2026-01-11 15:09:51 +0100

    Frontend: Remove 'auteur' attribute from Publication model

--- Lab/src/Models/Publication.ts
+++ Lab/src/Models/Publication.ts
@@ -4,6 +4,6 @@ export interface Publication {
   type: string; // Type de publication (article, conférence, livre, etc.)
   dateApparition: string; // Format ISO: "yyyy-MM-dd"
   lien: string; // URL ou DOI
-  auteurs: string; // Liste des auteurs séparés par des virgules
+  // auteurs: string; // Liste des auteurs séparés par des virgules
   source: string; // Journal, conférence, etc.
 }
